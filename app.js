e2b699465966e2805a3eee60dac61826425cdbff
Jaakko Hurtta <jaakko.hurtta@outlook.com>
2021-09-23 22:49:54 +0300

    workflow tweaks

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('v11.15 in dev branch')
+  res.send('v0.11.1 in master branch')
 })
 
 app.listen(PORT, () => {
