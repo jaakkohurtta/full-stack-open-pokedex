5d91b17dff36cecf727e7410a3c1646eaa0d90db
Jaakko Hurtta <jaakko.hurtta@outlook.com>
2021-09-23 12:18:43 +0300

    C:/Program Files/Git/version route tweak

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('v11.15 in master branch')
+  res.send('v11.15 in dev branch')
 })
 
 app.listen(PORT, () => {
